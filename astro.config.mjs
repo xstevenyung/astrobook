f996662119e5b74de60db2db2fb1202984b6ecee
Steven Yung <stevenyung@fastmail.com>
2022-10-21 23:14:01 +0200

    WIP

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import solid from "@astrojs/solid-js";
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [solid(), vue()],
+});
---
+++ src/components/Card.tsx
@@ -0,0 +1,7 @@
+export function Card() {
+  return (
+    <div>
+      <h2>Card</h2>
+    </div>
+  );
+}
